640fc8fd02b9a88c81858fc1844611e65f6da353
boskyle <boskyle.orendain@gmail.com>
2021-03-31 19:36:45 -0400

    slight cnages

--- covid19-tracker-canada/src/components/Super/Super.js
+++ covid19-tracker-canada/src/components/Super/Super.js
@@ -10,8 +10,12 @@ class Super extends Component {
     render() {
         return (
             <div className="mainContainer containerFluid d-flex align-items-center justify-content-center flex-column flex-xl-row">
-            <Navigation/>
-            <h2 className="border h-25">sup</h2>
+            {/* <Navigation/> */}
+            <div className="mapContainer d-flex flex-column align-items-center card" style={{marginBottom:'10%',backgroundColor:'#6C8AD7'}}>
+
+
+
+            </div>
             </div>
         );
     }
